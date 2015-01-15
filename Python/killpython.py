f98b8ef47c09c26caab879e4ea3f2cab3448adde
GisKook <zhangkai.gis@163.com>
2015-01-14 23:33:01 -0800

    kill python program

---
+++ Python/killpython.py
@@ -0,0 +1,51 @@
+#! /usr/bin/python
+#killpython.py
+import sys
+import subprocess
+import StringIO
+
+def listprograms(processname):
+    command = ['ps', '-ef']
+    p = subprocess.Popen(command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    buf = StringIO.StringIO(out)
+    havepid = 0
+    for line in buf:
+        if line.find(processname) != -1: 
+            sys.stdout.write("    ****  ")
+            print line
+
+if len(sys.argv) < 2:
+    print '''
+    ****  which python program do you want to kill? ;)
+    '''
+    listprograms("python")
+    exit()
+
+processname = sys.argv[1]
+processname = "python "+processname
+
+
+def main(): 
+    command = ['ps', '-ef']
+    p = subprocess.Popen(command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    buf = StringIO.StringIO(out)
+    havepid = 0
+    for line in buf:
+        if line.find(processname) != -1: 
+            pid = line.split(" ")[5]
+            subprocess.call(["kill",pid])
+            havapid = 1
+            print "kill this pid "+processname+"*"
+    if havepid == 0:
+        print "not find \"python "+processname+"*"
+
+
+
+if __name__ == "__main__":
+    main()
