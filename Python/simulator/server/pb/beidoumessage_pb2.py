aa79ba88ebbb712e522e04e6b5fd4b19213533fb
GisKook <zhangkai.gis@163.com>
2015-01-15 22:33:45 -0800

    a spelling mistake.

--- Python/simulator/server/pb/beidoumessage_pb2.py
+++ Python/simulator/server/pb/beidoumessage_pb2.py
@@ -13,7 +13,7 @@ from google.protobuf import descriptor_pb2
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='beidoumessage.proto',
   package='',
-  serialized_pb='\n\x13\x62\x65idoumessage.proto\"\xf9\x01\n\rCommunication\x12\x13\n\x0bmessageform\x18\x01 \x01(\r\x12\x17\n\x0fmessagecategory\x18\x02 \x01(\r\x12\x12\n\nencryption\x18\x03 \x01(\r\x12\x10\n\x08sendaddr\x18\x04 \x01(\r\x12\x10\n\x08recvaddr\x18\x05 \x01(\r\x12\x15\n\rsendtime_hour\x18\x06 \x01(\r\x12\x17\n\x0fsendtime_minute\x18\x07 \x01(\r\x12\x17\n\x0fsendtime_second\x18\x08 \x01(\r\x12\x15\n\rmessagelength\x18\t \x01(\r\x12\x0b\n\x03key\x18\n \x01(\x0c\x12\x15\n\rmessagebuffer\x18\x0b \x01(\x0c\"\xed\x03\n\x0cPositioninfo\x12\x0e\n\x06userid\x18\x01 \x01(\r\x12\x12\n\nencryption\x18\x02 \x01(\r\x12\x10\n\x08\x61\x63\x63uracy\x18\x03 \x01(\r\x12\x18\n\x10\x65mergencypostion\x18\x04 \x01(\r\x12\x1a\n\x12multivaluesolution\x18\x05 \x01(\r\x12\x0b\n\x03key\x18\x06 \x01(\x0c\x12\x17\n\x0f\x61pplaytime_hour\x18\x07 \x01(\r\x12\x19\n\x11\x61pplaytime_minute\x18\x08 \x01(\r\x12\x19\n\x11\x61pplaytime_second\x18\t \x01(\r\x12\x19\n\x11\x61pplaytime_tenths\x18\n \x01(\r\x12\x18\n\x10longitude_degree\x18\x0b \x01(\r\x12\x18\n\x10longitude_minute\x18\x0c \x01(\r\x12\x18\n\x10longitude_second\x18\r \x01(\r\x12\x18\n\x10longitude_tenths\x18\x0e \x01(\r\x12\x17\n\x0flatitude_degree\x18\x0f \x01(\r\x12\x17\n\x0flatitude_minute\x18\x10 \x01(\r\x12\x17\n\x0flatitude_second\x18\x11 \x01(\r\x12\x17\n\x0flatitude_tenths\x18\x12 \x01(\r\x12\x16\n\x0egeodeticheight\x18\x13 \x01(\x05\x12\x16\n\x0e\x64\x65tlaelevation\x18\x14 \x01(\x05\"\x8c\x01\n\x14\x43ommunicationreceipt\x12\x10\n\x08sendaddr\x18\x01 \x01(\r\x12\x10\n\x08recvaddr\x18\x02 \x01(\r\x12\x18\n\x10receipttime_hour\x18\x03 \x01(\r\x12\x1a\n\x12receipttime_minute\x18\x04 \x01(\r\x12\x1a\n\x12receipttime_second\x18\x05 \x01(\r\"\xa5\x01\n\rBeidoumessage\x12\x13\n\x0bmessagetype\x18\x01 \x01(\r\x12%\n\rcommuincation\x18\x02 \x01(\x0b\x32\x0e.Communication\x12\x33\n\x14\x63ommunicationreceipt\x18\x03 \x01(\x0b\x32\x15.Communicationreceipt\x12#\n\x0cpositioninfo\x18\x04 \x01(\x0b\x32\r.Positioninfo')
+  serialized_pb='\n\x13\x62\x65idoumessage.proto\"\xf9\x01\n\rCommunication\x12\x13\n\x0bmessageform\x18\x01 \x01(\r\x12\x17\n\x0fmessagecategory\x18\x02 \x01(\r\x12\x12\n\nencryption\x18\x03 \x01(\r\x12\x10\n\x08sendaddr\x18\x04 \x01(\r\x12\x10\n\x08recvaddr\x18\x05 \x01(\r\x12\x15\n\rsendtime_hour\x18\x06 \x01(\r\x12\x17\n\x0fsendtime_minute\x18\x07 \x01(\r\x12\x17\n\x0fsendtime_second\x18\x08 \x01(\r\x12\x15\n\rmessagelength\x18\t \x01(\r\x12\x0b\n\x03key\x18\n \x01(\x0c\x12\x15\n\rmessagebuffer\x18\x0b \x01(\x0c\"\xe9\x03\n\x0cPositioninfo\x12\x0e\n\x06userid\x18\x01 \x01(\r\x12\x12\n\nencryption\x18\x02 \x01(\r\x12\x10\n\x08\x61\x63\x63uracy\x18\x03 \x01(\r\x12\x18\n\x10\x65mergencypostion\x18\x04 \x01(\r\x12\x1a\n\x12multivaluesolution\x18\x05 \x01(\r\x12\x0b\n\x03key\x18\x06 \x01(\x0c\x12\x16\n\x0e\x61pplytime_hour\x18\x07 \x01(\r\x12\x18\n\x10\x61pplytime_minute\x18\x08 \x01(\r\x12\x18\n\x10\x61pplytime_second\x18\t \x01(\r\x12\x18\n\x10\x61pplytime_tenths\x18\n \x01(\r\x12\x18\n\x10longitude_degree\x18\x0b \x01(\r\x12\x18\n\x10longitude_minute\x18\x0c \x01(\r\x12\x18\n\x10longitude_second\x18\r \x01(\r\x12\x18\n\x10longitude_tenths\x18\x0e \x01(\r\x12\x17\n\x0flatitude_degree\x18\x0f \x01(\r\x12\x17\n\x0flatitude_minute\x18\x10 \x01(\r\x12\x17\n\x0flatitude_second\x18\x11 \x01(\r\x12\x17\n\x0flatitude_tenths\x18\x12 \x01(\r\x12\x16\n\x0egeodeticheight\x18\x13 \x01(\x05\x12\x16\n\x0e\x64\x65tlaelevation\x18\x14 \x01(\x05\"\x8c\x01\n\x14\x43ommunicationreceipt\x12\x10\n\x08sendaddr\x18\x01 \x01(\r\x12\x10\n\x08recvaddr\x18\x02 \x01(\r\x12\x18\n\x10receipttime_hour\x18\x03 \x01(\r\x12\x1a\n\x12receipttime_minute\x18\x04 \x01(\r\x12\x1a\n\x12receipttime_second\x18\x05 \x01(\r\"\xa5\x01\n\rBeidoumessage\x12\x13\n\x0bmessagetype\x18\x01 \x01(\r\x12%\n\rcommuincation\x18\x02 \x01(\x0b\x32\x0e.Communication\x12\x33\n\x14\x63ommunicationreceipt\x18\x03 \x01(\x0b\x32\x15.Communicationreceipt\x12#\n\x0cpositioninfo\x18\x04 \x01(\x0b\x32\r.Positioninfo')
 
 
 
@@ -166,28 +166,28 @@ _POSITIONINFO = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='applaytime_hour', full_name='Positioninfo.applaytime_hour', index=6,
+      name='applytime_hour', full_name='Positioninfo.applytime_hour', index=6,
       number=7, type=13, cpp_type=3, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='applaytime_minute', full_name='Positioninfo.applaytime_minute', index=7,
+      name='applytime_minute', full_name='Positioninfo.applytime_minute', index=7,
       number=8, type=13, cpp_type=3, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='applaytime_second', full_name='Positioninfo.applaytime_second', index=8,
+      name='applytime_second', full_name='Positioninfo.applytime_second', index=8,
       number=9, type=13, cpp_type=3, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='applaytime_tenths', full_name='Positioninfo.applaytime_tenths', index=9,
+      name='applytime_tenths', full_name='Positioninfo.applytime_tenths', index=9,
       number=10, type=13, cpp_type=3, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
@@ -273,7 +273,7 @@ _POSITIONINFO = _descriptor.Descriptor(
   is_extendable=False,
   extension_ranges=[],
   serialized_start=276,
-  serialized_end=769,
+  serialized_end=765,
 )
 
 
@@ -328,8 +328,8 @@ _COMMUNICATIONRECEIPT = _descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=772,
-  serialized_end=912,
+  serialized_start=768,
+  serialized_end=908,
 )
 
 
@@ -377,8 +377,8 @@ _BEIDOUMESSAGE = _descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=915,
-  serialized_end=1080,
+  serialized_start=911,
+  serialized_end=1076,
 )
 
 _BEIDOUMESSAGE.fields_by_name['commuincation'].message_type = _COMMUNICATION
