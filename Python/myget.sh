8d3627913133c243a728ff37c9535820d01053d0
Giskook <zhangkai.gis@163.com>
2015-12-05 12:14:41 +0000

    add webbrowser

---
+++ Python/myget.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget --recursive --no-clobber --page-requisites --convert-links --html-extension --restrict-file-names=windows --no-parent $1
---
+++ Python/webbrowser.py
@@ -0,0 +1,39 @@
+__author__ = 'zhangkai'
+import urllib2
+import subprocess
+
+url = "http://www.google.com"
+localurl="./www.google.com/index.html"
+
+def wgetbrowser():
+    command=['/bin/bash','./myget.sh',url]
+    subprocess.call(command)
+
+def openbrowseronline():
+    command=['chromium', '--kiosk', url]
+    subprocess.call(command)
+
+def openbrowserlocal():
+    command=['chromium', '--kiosk', localurl]
+    subprocess.call(command)
+
+
+def main():
+    showonline = 1
+    try:
+        urllib2.urlopen(url=url,timeout=10,)
+    except urllib2.HTTPError, err:
+        print err
+        showonline = 0
+    except urllib2.URLError, err:
+        print err
+        showonline = 0
+
+    if showonline==1 :
+        wgetbrowser()
+        openbrowseronline()
+    else:
+	openbrowserlocal()
+
+if __name__ == "__main__":
+    main()
